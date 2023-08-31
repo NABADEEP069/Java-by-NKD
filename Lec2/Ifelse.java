f652939a7345a5daa931706a5144010236331bae
NABADEEP <nabadeepdas069@gmail.com>
2023-08-31 06:54:13 +0530

    initail commit

---
+++ Lec2/Ifelse.java
@@ -0,0 +1,15 @@
+package Lec2;
+
+public class Ifelse {
+    public static void main(String[] args) {
+      int age = 30;
+    if (age > 18) 
+    {
+        System.out.println("this is an adult");
+    }
+    else 
+    {
+        System.out.println("this is not an adult");
+    }   
+    }
+}
